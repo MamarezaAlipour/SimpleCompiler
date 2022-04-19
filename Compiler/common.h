9cf07beba962dc44111e73717a771529aba18555
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-19 21:06:05 +0430

    const modify

--- Compiler/common.h
+++ Compiler/common.h
@@ -12,25 +12,25 @@ typedef unsigned int uint;
 typedef unsigned long long ulong;
 
 template <class A, class B>
-std::list<B>& operator<<(std::list<B>& l, const A& e) {
+std::list<B>& operator<<(std::list<B>& l, A const& e) {
 	l.push_back((B)e);
 	return l;
 }
 
 template <class A, class B>
-std::list<B>* operator<<(std::list<B>* l, const A& e) {
+std::list<B>* operator<<(std::list<B>* l, A const& e) {
 	l->push_back((B)e);
 	return l;
 }
 
 template <class A, class B>
-std::vector<B>& operator<<(std::vector<B>& v, const A& e) {
+std::vector<B>& operator<<(std::vector<B>& v, A const& e) {
 	v.push_back((B)e);
 	return v;
 }
 
 template <class A, class B>
-std::vector<B>* operator<<(std::vector<B>* v, const A& e) {
+std::vector<B>* operator<<(std::vector<B>* v, A const& e) {
 	v->push_back((B)e);
 	return v;
 }
@@ -55,7 +55,7 @@ T fromString(const std::string& str) {
 }
 
 template <>
-inline double fromString<double>(const std::string& str) {
+inline double fromString<double>(std::string const& str) {
 	if (str == "inf")
 		return INFINITY;
 
